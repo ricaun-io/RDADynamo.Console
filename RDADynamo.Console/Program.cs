9b2279d58ef89131aa4754ed48ae5e3beeee95d2
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-08-29 16:18:35 -0300

    Add project files.

---
+++ RDADynamo.Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using Autodesk.Forge.Oss.DesignAutomation;
+using Autodesk.Forge.Oss.DesignAutomation.Attributes;
+using Autodesk.Forge.Oss.DesignAutomation.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RDADynamo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run(async () =>
+            {
+                await RunDynamo();
+            });
+            task.GetAwaiter().GetResult();
+        }
+
+        private static async Task RunDynamo()
+        {
+
+            IDesignAutomationService designAutomationService = new RevitDesignAutomationService("RDADynamo")
+            {
+                EngineVersions = new[] { "2024" },
+                EnableConsoleLogger = true,
+                ForceUpdateActivity = true,
+                //ForceCreateWorkItemReport = true,
+                EnableReportConsoleLogger = true,
+                ForgeEnvironment = "test",
+            };
+
+            if (File.Exists("RDADynamo.bundle.zip") == false)
+            {
+                Console.WriteLine("Download RDADynamo.bundle.zip");
+                var bundle = await RequestService.Instance.GetFileAsync("https://github.com/tothom/RDADynamo-example-implementation/releases/download/0.0.1/RDADynamo.bundle.zip");
+            }
+
+            await designAutomationService.Initialize("RDADynamo.bundle.zip");
+
+            var sampleFolder = "set-parameter";
+
+            await designAutomationService.Run<DynamoParameters>((parameters) =>
+            {
+                parameters.RevitFile = @$".\samples\{sampleFolder}\sample.rvt";
+                parameters.DynamoInput = @$".\samples\{sampleFolder}\input.zip";
+            });
+
+            //await designAutomationService.Delete();
+        }
+    }
+
+    internal class DynamoParameters
+    {
+        [ParameterActivityInputOpen]
+        [ParameterInput("rvtFile.rvt", Required = true)]
+        public string RevitFile { get; set; }
+        [ParameterInput("input.zip", Required = true, Zip = true)]
+        public string DynamoInput { get; set; }
+        [ParameterOutput("result", Zip = true, DownloadFile = true)]
+        public string DynamoOutput { get; set; }
+
+        [ParameterOutput("result.rvt", DownloadFile = true)]
+        public string RevitOutputFile { get; set; }
+    }
+}
---
+++ RDADynamo.Console/samples/set-parameter/README.md
@@ -0,0 +1,54 @@
+﻿Dynamo graph which writes a combined mark parameter value to all doors in the model. The graph gets all doors, finds out which room they belong. The room number is combined with the door mark and written to a parameter in the door. The revit model is saved as a new file with name `result.rvt`, which needs to be uploaded after the the Design Automation has completed.
+
+The name of the combined mark parameter can be changed like this in the `input.json` file:
+```json
+[
+  {
+    "GraphName": "Mark Doors.dyn",
+    "InputFolder": "input.zip",
+    "ResultFolder": "result",
+    "InputFileNames": [
+    ],
+    "Packages": [
+    ],
+    "NodeInput": [
+      {
+        "Id": "0c302edfd35c494d823990676f5f4aa0",
+        "Name": "Value",
+        "Value": "CombinedMark"
+      },
+    ]
+  }
+]
+```
+## Get the Bim360/ACC file storage id for use in Design Automation
+
+<aside>
+☝ The `ProjectId` must be prefixed with `b.` That is b with a dot afterwards
+Example:
+
+`5ce30d0c-002d-42bb-8c00-661c0689ec41`
+
+becomes
+
+`b.5ce30d0c-002d-42bb-8c00-661c0689ec41`
+
+</aside>
+
+1. Get item **URN**
+    1. The URN is just the `id` when querying the items in a folder
+    2. You can also get it directly from the URL in BIM360 Docs. For example, it’s the last part of the URL when viewing a model
+        
+        ```html
+        https://docs.b360.autodesk.com/projects/5ce30d0c-002d-42bb-8c00-661c0689ec41/folders/urn:adsk.wipprod:fs.folder:co.I_OxfDFsTFazBTR9Gz7mzQ/detail/viewer/items/urn:adsk.wipprod:dm.lineage:Gxrge9LfQXS0LFrW2sq8_Q
+        ```
+        which results in
+
+        `urn:adsk.wipprod:dm.lineage:Gxrge9LfQXS0LFrW2sq8_Q`
+        
+2. Get the **Storage Id**
+    1. This Id is located in the response body of a query to
+        1. https://developer.api.autodesk.com/data/v1/projects/`projectID`/items/`itemUrn`/tip
+        2. *Tip* means the latest version
+    2. The Storage Id is located here in the response body:
+        1. `responseBody.data.relationships.storage.data.id`
